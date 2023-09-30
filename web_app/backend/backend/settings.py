42ca7a580cadd5b057ccc9196122e244c8e3d2b6
piotr-bledowski-student <63125128+piotr-bledowski@users.noreply.github.com>
2023-09-30 10:51:47 +0200

    React - Django integration done

--- web_app/backend/backend/settings.py
+++ web_app/backend/backend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api',
+    'base',
 ]
 
 MIDDLEWARE = [
@@ -123,6 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, '../frontend/build/static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
