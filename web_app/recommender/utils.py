793ef53c0a02b7ec6f4a61b56b50a8eebd2f0080
Wojciech Lapacz <wojciech.lapacz02@gmail.com>
2023-09-30 20:59:20 +0200

    get descriptions for majors

--- web_app/recommender/utils.py
+++ web_app/recommender/utils.py
@@ -1,6 +1,21 @@
 import torch
+import pandas as pd
+import os
 
 
 def create_single_embedding(description: str, model):
     embedding = model.encode(description)
     return torch.tensor(embedding)
+
+
+def get_major_descriptions(majors):
+    df = pd.read_csv(os.path.join(os.path.curdir, "data", "kierunki_studiow.csv"))
+    result = []
+    for major in majors:
+        desc_list = df[df["nazwa"] == major + " "]["opis"].iloc[0].split('.')
+        if len(desc_list) > 5:
+            desc = ".".join(desc_list[:5]) + '.'
+        else:
+            desc = ".".join(desc_list) + '.'
+        result.append(desc)
+    return result
