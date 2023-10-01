d885106b53ac5248ffb0f1cb0100b73ace47ea0d
Eva Reszka <e.reszka@stud.elka.pw.edu.pl>
2023-10-01 02:12:21 +0200

    Add results page

--- web_app/frontend/src/App.js
+++ web_app/frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import WelcomeView from './components/welcomeView';
 import UniToolView from "./components/uniToolView";
+import UniToolOutput from "./components/uniToolOutput";
 import { Route, Routes } from "react-router-dom"
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Routes>
         <Route path="/" element={<WelcomeView/>}/>
         <Route path="/uni-tool" element={<UniToolView/>}/>
+        <Route path="/results" element={<UniToolOutput/>}/>
     </Routes>
   );
 }
