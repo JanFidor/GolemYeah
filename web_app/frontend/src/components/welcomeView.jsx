a6a0b4a94be553db3ee5c6a99537c24b119af5f3
Eva Reszka <e.reszka@stud.elka.pw.edu.pl>
2023-10-01 11:13:18 +0200

    Update hover buttons

--- web_app/frontend/src/components/welcomeView.jsx
+++ web_app/frontend/src/components/welcomeView.jsx
@@ -13,8 +13,16 @@ const WelcomeView = () => {
         <div className="welcome-title">
             Jak widzisz swoją przyszłość?
         </div>
-            <div className={"button-div"}>
             <button className={"button-tool"} onClick={handleClick}>Przejdź do narzędzia</button>
+            <div className={"button-div"}>
+
+            </div>
+            <div className={"app-descr"}>
+                Skorzystaj z naszego narzędzia pomagającego w wybraniu kierunku studiów. Pytania będą mieć charakter otwarty i zamknięty, na im więcej z nich odpowiesz – tym dokładniejszy otrzymasz wynik.
+            </div>
+
+            <div className={"app-descr"}>
+                Po odpowiedzeniu na pytania zostanie dla ciebie przygotowana lista proponowanych kierunków studiów i uczelnie, które mają je w swojej ofercie. Im wyżej ulokowany na liście jest kierunek studiów – tym więcej wspólnego ma z twoimi preferencjami. Po kliknięciu w nazwę kierunku wyświetli się jego skrócony opis i uczelnie oferujące ten kierunek.
             </div>
         <Footer/>
         </>
